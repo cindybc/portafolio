c56c772962b9765b7eb1263d87170f6545d07500
cindy <cindialy@gmail.com>
2017-08-28 10:40:26 -0300

    avance js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
- 
+  $(document).ready(function(){
+  	//Parallax
+  	 $('.parallax').parallax();
+
+
+    //Menú desplegable 
+  $(".button-collapse").sideNav({
+  	closeOnClick: true,
+    menuWidth: 250,
+  });
+
+  // Scrollspy
+   $('.scrollspy').scrollSpy();
+
+  // Desplazamiento
+    $('a[href*="#"]').click(function(e){				
+		e.preventDefault();
+			$('body,html').stop(true,true).animate({				
+				scrollTop: $(this.hash).offset().top
+			},1000);
+		
+	});
+
+
+  });
+     
+
+  
+
 
